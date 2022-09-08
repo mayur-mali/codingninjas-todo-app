58171b10d85de6baa659d280b3e0d1aefbc38a32
mayur-mali <mayurmali31@gmail.com>
2022-09-08 21:23:42 +0530

    create app and done basic routing setup

---
+++ index.js
@@ -0,0 +1,13 @@
+const { application } = require("express");
+const express = require("express");
+const app = express();
+const port = 8000;
+
+app.get("/", require("./routes"));
+
+app.listen(port, function (err) {
+  if (err) {
+    console.log(`somthing error while running server: ${err}`);
+  }
+  console.log(`server running on port: ${port}`);
+});
