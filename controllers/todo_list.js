da51b124aebe48e0f14bd96e529ea591166c7b3c
mayur-mali <mayurmali31@gmail.com>
2022-09-08 22:13:30 +0530

    server created

---
+++ controllers/todo_list.js
@@ -0,0 +1,3 @@
+module.exports.todo = function (req, res) {
+  return res.end("<h1>hello world!</h1>");
+};
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
-router.get("/", function (req, res) {
-  return res.end("<h1>hello world </h1>");
-});
+const todoController = require("../controllers/todo_list");
+router.get("/", todoController.todo);
 
 module.exports = router;
